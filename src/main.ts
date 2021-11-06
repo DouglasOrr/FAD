6a5a577dda3bb106c90efe8c23beed5a42a5a3df
Douglas Orr <douglas.orr99@gmail.com>
2021-11-06 23:50:21 +0000

    AudioBuffer and StereoPanner

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,55 @@ export function name() {
     return "ping";
 }
 
+///////////////////////////////////////////////////////////////////////////////
+// Audio
+
+class Player {
+    context: AudioContext;
+    ping0: AudioBuffer;
+
+    constructor(context: AudioContext) {
+        this.context = context;
+        const player = this;
+        async function fetchPing() {
+            let response = await fetch("assets/ping0.mp3");
+            let buffer = await response.arrayBuffer();
+            player.ping0 = await context.decodeAudioData(buffer);
+        };
+        fetchPing()
+    }
+
+    play(pan: number): void {
+        const source = this.context.createBufferSource();
+        source.buffer = this.ping0;
+        const panNode = this.context.createStereoPanner();
+        source.connect(panNode);
+        panNode.pan.value = pan;
+        panNode.connect(this.context.destination);
+        source.start();
+    }
+};
+
+///////////////////////////////////////////////////////////////////////////////
+// Drawing
+
+type DrawFn = (context: CanvasRenderingContext2D) => void;
+
+function setupCanvas(canvas: HTMLCanvasElement, draw: DrawFn) {
+    const context = canvas.getContext("2d");
+    canvas.width = canvas.parentElement.clientWidth;
+    canvas.height = canvas.parentElement.clientHeight;
+    draw(context);
+    window.addEventListener("resize", () => {
+        canvas.width = canvas.parentElement.clientWidth;
+        canvas.height = canvas.parentElement.clientHeight;
+        draw(context);
+    });
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Startup
+
 window.onload = () => {
     const params = new URLSearchParams(window.location.search);
     if (params.has("autoreload")) {
@@ -11,9 +60,18 @@ window.onload = () => {
         document.head.appendChild(script);
     }
 
+    const player = new Player(new window.AudioContext());
+
     document.onkeydown = (e: KeyboardEvent) => {
         if (e.key === " ") {
-            new Audio("assets/ping0.mp3").play()
+            player.play(-((document.getElementById("pan") as HTMLInputElement).value));
         }
     };
+
+    setupCanvas(document.getElementById("screen") as HTMLCanvasElement,
+        (context: CanvasRenderingContext2D) => {
+            context.fillStyle = "#000000";
+            context.fillRect(0, 0, context.canvas.clientWidth, context.canvas.clientHeight);
+        }
+    );
 };
