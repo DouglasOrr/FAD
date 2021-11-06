3910eb90fd8e5c03dfd90be2a4864a6ab1818d46
Douglas Orr <douglas.orr99@gmail.com>
2021-11-06 19:16:23 +0000

    Add asset syncing (fixes #3)

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,15 @@ export function name() {
 window.onload = () => {
     const params = new URLSearchParams(window.location.search);
     if (params.has("autoreload")) {
-        console.log("Loading + autoreload");
+        console.log("Autoreloading");
         const script = document.createElement("script");
         script.src = "https://livejs.com/live.js";
         document.head.appendChild(script);
     }
-}
+
+    document.onkeydown = (e: KeyboardEvent) => {
+        if (e.key === " ") {
+            new Audio("assets/ping0.mp3").play()
+        }
+    };
+};
