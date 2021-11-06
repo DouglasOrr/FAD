e2026a99a6ceefb6cafc49596a23c1dbfe5c1a68
Douglas Orr <douglas.orr99@gmail.com>
2021-11-06 18:11:03 +0000

    Only load livejs script in dev builds (fixes #2)

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,12 @@ export function name() {
     return "ping";
 }
 
-console.log(`Running ${name()}`);
+window.onload = () => {
+    const params = new URLSearchParams(window.location.search);
+    if (params.has("autoreload")) {
+        console.log("Loading + autoreload");
+        const script = document.createElement("script");
+        script.src = "https://livejs.com/live.js";
+        document.head.appendChild(script);
+    }
+}
