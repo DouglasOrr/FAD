c4ae950af5b2c8d1796720e87e0db6c732d5e008
Douglas Orr <douglas.orr99@gmail.com>
2021-11-14 13:01:50 +0000

    Add some fake echoes to 'ping' and use AudioNode constructors as recommended by MDN

--- src/ts/sound.ts
+++ src/ts/sound.ts
@@ -15,35 +15,41 @@ export class Player {
     }
 
     pingDemo(pan: number): void {
-        const source = this.context.createBufferSource();
-        source.buffer = this.ping0;
-        const panNode = this.context.createStereoPanner();
-        panNode.pan.value = pan;
-        source.connect(panNode).connect(this.context.destination);
+        const source = new AudioBufferSourceNode(this.context, { buffer: this.ping0 });
+        source.connect(new StereoPannerNode(this.context, { pan: pan })).connect(this.context.destination);
         source.start();
     }
 
     private decay(value: number, duration: number): GainNode {
-        const node = this.context.createGain();
+        const node = new GainNode(this.context);
         node.gain.value = value;
         node.gain.exponentialRampToValueAtTime(value / 10, this.context.currentTime + duration);
         return node
     }
 
+    private addEcho(source: AudioNode, delay: number, gain: number): void {
+        source
+            .connect(new DelayNode(this.context, { delayTime: delay, maxDelayTime: delay }))
+            .connect(new GainNode(this.context, { gain: gain }))
+            .connect(this.context.destination);
+    }
+
     ping(): void {
-        const oscillator = this.context.createOscillator();
-        oscillator.frequency.value = 1000;
-        oscillator.type = "sine";
-        oscillator.connect(this.decay(1.0, 0.1)).connect(this.context.destination);
+        const oscillator = new OscillatorNode(this.context, { type: "sine", frequency: 1000 });
+
+        const decay = oscillator.connect(this.decay(1.0, 0.1));
+        decay.connect(this.context.destination);
+        this.addEcho(decay, 0.2, 0.5);
+        this.addEcho(decay, 0.4, 0.1);
+
         oscillator.start();
         oscillator.stop(this.context.currentTime + 0.1);
     }
 
     collision(): void {
-        const oscillator = this.context.createOscillator();
+        const oscillator = new OscillatorNode(this.context, { type: "triangle" });
         oscillator.frequency.setValueAtTime(200, this.context.currentTime);
         oscillator.frequency.exponentialRampToValueAtTime(100, this.context.currentTime + 0.5);
-        oscillator.type = "triangle";
         oscillator.connect(this.decay(1.0, 0.5)).connect(this.context.destination);
         oscillator.start();
         oscillator.stop(this.context.currentTime + 0.5);
@@ -52,9 +58,7 @@ export class Player {
     finished(): void {
         const baseFrequency = 440;
         for (const frequency of [baseFrequency, baseFrequency * 1.5, baseFrequency * 2]) {
-            const oscillator = this.context.createOscillator();
-            oscillator.frequency.value = frequency;
-            oscillator.type = "triangle";
+            const oscillator = new OscillatorNode(this.context, { type: "triangle", frequency: frequency });
             oscillator.connect(this.decay(0.5, 1.0)).connect(this.context.destination);
             oscillator.start();
             oscillator.stop(this.context.currentTime + 1.0);
